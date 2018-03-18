e6c9f59c45d76c2900b17a38bd66ebab526d8b58
Vijay Deepak <vijaydeepak7195@outlook.com>
2018-03-18 17:37:18 +0530

    commit - 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,39 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  bgColor: string = 'white';
+
+  constructor(
+    public navCtrl: NavController,
+    private speechRecognition: SpeechRecognition
+  ) {
+
+  }
+
+  ngOnInit() {
+
+    this.speechRecognition.hasPermission()
+      .then((has: boolean) => {
+        if(!has) {
+          this.speechRecognition.requestPermission()
+            .then(
+              () => console.log('granted'),
+              () => console.log('denied')
+            );
+        }
+      });
+
+  }
+
+  start() {
+
+    this.speechRecognition.startListening()
+      .subscribe(
+        (matches: Array<string>) => {
+          console.log(matches);
+          this.bgColor = matches[0];
+        }
+      );
 
   }
 
